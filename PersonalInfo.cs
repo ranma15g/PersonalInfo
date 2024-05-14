64be94b9ca0af2277a20c3f2b998087892813190
ranma15g <ranma15g@gmail.com>
2024-05-14 18:43:48 +0000

    Initial commit

---
+++ PersonalInfo.cs
@@ -0,0 +1,20 @@
+/*
+1-1.  Write, compile, and test a program named PersonalInfo that displays a person’s name, birthdate, work phone number, and cell phone number on separate lines.
+*/
+
+using System;
+
+public class Program 
+{
+  public static void Main(string[] args)
+  {
+    // Unit Test #1 Name
+    Console.WriteLine("Lucinda Potter");
+    // Unit Test #1 Birthdate
+    Console.WriteLine("6/24/1992");
+    // Unit Test #1 Work Phone Number
+    Console.WriteLine("work 000-000-0101");
+    // Unit Test #1 Cell Phone Number
+    Console.WriteLine("cell 000-000-0189");
+  }
+}
